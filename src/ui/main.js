51bc340145abfdcde8c90eb73df02935539e6ae6
Sanidhya Khajuria <khajuri@amazon.com>
2023-06-14 22:04:15 +0530

    Added electron to run in development mode

---
+++ src/ui/main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  mainWindow.loadURL('http://localhost:3000');
+}
+
+// Add this line
+app.commandLine.appendSwitch('disable-web-security');
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
